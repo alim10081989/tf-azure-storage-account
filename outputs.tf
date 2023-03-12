f45b6b85d58ae037cf7a3e703e1801dab500e3bf
alim10081989 <alim.azad@gmail.com>
2023-03-12 10:13:47 +0530

    Isolated Access key output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-# Output declarations
 output "storage_account_name" {
   description = "Storage Accout Name"
   value       = azurerm_storage_account.tfstate.name
@@ -15,27 +14,29 @@ output "win_container_blob_name" {
 }
 
 output "unix_vm_backend_config" {
-    value = <<EOT
+  value = <<EOT
         backend "azurerm" {
             resource_group_name  = "${azurerm_resource_group.tfstate.name}"
             storage_account_name = "${azurerm_storage_account.tfstate.name}"
             container_name       = "${azurerm_storage_container.unix-tfstate.name}"
             key                  = "terraform.tfstate"
-            access_key           = "${azurerm_storage_account.tfstate.primary_access_key}"
   }
     EOT
-    sensitive = true
 }
 
 output "win_vm_backend_config" {
-    value = <<EOT
+  value = <<EOT
         backend "azurerm" {
             resource_group_name  = "${azurerm_resource_group.tfstate.name}"
             storage_account_name = "${azurerm_storage_account.tfstate.name}"
             container_name       = "${azurerm_storage_container.win-tfstate.name}"
             key                  = "terraform.tfstate"
-            access_key           = "${azurerm_storage_account.tfstate.primary_access_key}"
   }
     EOT
-    sensitive = true
+}
+
+output "storage_account_access_key" {
+  description = "Storage account primary key"
+  value       = azurerm_storage_account.tfstate.primary_access_key
+  sensitive   = true
 }
